c33027089925877b6300eca8692dc7c2b6ad7743
Davon Burks <burksdavon29@yahoo.com>
2024-02-14 23:14:12 -0500

    Still working on Tools Design

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -1,10 +1,12 @@
+import Dropdown from "./components/category-dropdown";
 import Filters from "./components/filters";
 
 export default function Tools() {
   return (
     <main className="">
       <h1 className="text-center text-4xl font-bold my-10"> WEBMASTERS TOOLS LIST</h1>
-      <Filters label={""} onClose={function (): void {
+      <Dropdown label={"Kobe"} options={[]} />
+      <Filters label={"Web App"} onClose={function (): void {
         throw new Error("Function not implemented.");
       } } />
     </main>
