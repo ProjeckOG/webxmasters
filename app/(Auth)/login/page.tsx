f94949aaa9120a7130860933b7ddcfc505a2645f
Davon Burks <burksdavon29@yahoo.com>
2024-02-28 16:55:11 -0500

    Fixing Tools page

--- app/(Auth)/login/page.tsx
+++ app/(Auth)/login/page.tsx
@@ -20,13 +20,13 @@ import { Input } from "@/lib/@/components/ui/input";
 
 export default async function Login() {
   const formSchema = z.object({
-    username: z.string().min(2).max(50),
+    email: z.string().min(2).max(50),
   });
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      email: "",
     },
   });
 
--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -5,10 +5,7 @@ export default function Tools() {
   return (
     <main className="">
       <h1 className="text-center text-4xl font-bold my-10"> WEBMASTERS TOOLS LIST</h1>
-      <Dropdown label={"Kobe"} options={[]} />
-      <Filters label={"Web App"} onClose={function (): void {
-        throw new Error("Function not implemented.");
-      } } />
+     
     </main>
   )
 }
