1111bf3daf8e490874bd4a5218979e8d8cc1e22d
Davon Burks <burksdavon29@yahoo.com>
2024-03-10 21:33:22 -0400

    Created Support Page

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -1,10 +1,19 @@
-
+import { Button } from "@/lib/@/components/ui/button";
+import { MailIcon } from "lucide-react";
 
 export default function Support() {
-    return (
-      <main className="">
-        <h1 className="text-center text-4xl font-bold my-10"> SUPPORT</h1>
-      </main>
-    )
-  }
-  
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen border p-4 ">
+      <h1 className="text-4xl font-bold text-center text-white mb-6">
+        Need Help?
+      </h1>
+      <p className="text-lg text-center text-white mb-8">
+        Our team is here to assist you. If you have any questions or encounter
+        any issues, don't hesitate to reach out.
+      </p>
+      <Button className=" flex justify-center border font-bold p-10 uppercase hover:bg-primary-foreground mx-auto rounded-full  transition duration-300 ease-in-out transform hover:-translate-y-1">
+        <a href="mailto:support@webxmasters.io"> Contact Support</a>
+      </Button>
+    </div>
+  );
+}
