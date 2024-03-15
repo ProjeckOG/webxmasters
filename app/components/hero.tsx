53c9cf7757f0505e49c550ff16919dbd13cbc561
Davon Burks <burksdavon29@yahoo.com>
2024-03-14 22:49:02 -0400

    fixing hero buttons

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -9,6 +9,7 @@ import {
   ShoppingBag,
   Smartphone,
 } from "lucide-react";
+import Link from "next/link";
 const Hero = () => {
   return (
     <div className="w-3/4 md:w-3/4 mx-auto ">
@@ -16,55 +17,77 @@ const Hero = () => {
         Discover webmaster tools and get started building today
       </h1>
       <h2 className="text-center text-2xl mt-5">What do you want to build?</h2>
-      <div className="flex justify-center flex-wrap	 mx-auto w-full md:w-3/4">
-        <div className="bg-primary-color rounded-lg  flex mt-5  justify-center items-center mr-5 text-center w-60 h-60">
-          <Button  className="bg-secondary hover:border-2 hover:bg-background rounded-lg w-full h-full">
-            <PanelsTopLeft className="mx-auto "  size={50} />
-            WEBSITES
-          </Button>
-        </div>
-        <div className="bg-primary-color rounded-lg  flex mt-5 justify-center items-center  mr-5 text-center w-60 h-60">
-          <Button className="bg-secondary hover:border-2 hover:bg-background rounded-lg w-full h-full">
-            <AppWindow className="mx-auto" size={50} />
-            <h3 className="mt-2">WEB APPS</h3>
-          </Button>
-        </div>
-        <div className="bg-primary-color rounded-lg  flex mt-5  justify-center items-center  mr-5 text-center w-60 h-60">
-          <Button  className="bg-secondary hover:border-2 hover:bg-background rounded-lg w-full h-full">
-            <Smartphone className="mx-auto" size={50} />
-            <h3 className="mt-2">MOBILE APPS</h3>
-          </Button>
-        </div>
-        <div className="bg-primary-color rounded-lg  flex mt-5 justify-center items-center mr-5 text-center w-60 h-60">
-          <Button  className="bg-secondary hover:border-2 hover:bg-background rounded-lg w-full h-full">
-            <Mail className="mx-auto" size={50} />
-            <h3 className="mt-2">EMAIL</h3>
-          </Button>
-        </div>
-        <div className="bg-primary-color rounded-lg  flex mt-5 justify-center items-center  mr-5 text-center w-60 h-60">
-          <Button  className="bg-secondary hover:border-2 hover:bg-background rounded-lg w-full h-full">
-            <ShoppingBag className="mx-auto" size={50} />
-            <h3 className="mt-2">ECOMMERCE</h3>
-          </Button>
-        </div>
-        <div className="bg-primary-color rounded-lg  flex mt-5 justify-center items-center  mr-5 text-center w-60 h-60">
-          <Button  className="bg-secondary hover:border-2 hover:bg-background rounded-lg w-full h-full">
-            <Bot className="mx-auto" size={50} />
-            <h3 className="mt-2">ARTIFICIAL<br /> INTELLIGENCE</h3>
-          </Button>
-        </div>
-        <div className="bg-primary-color rounded-lg  flex mt-5 justify-center items-center  mr-5 text-center w-60 h-60">
-          <Button  className="bg-secondary hover:border-2 hover:bg-background rounded-lg w-full h-full">
-            <Server className="mx-auto" size={50} />
-            <h3 className="mt-2">WEBSITE <br/>HOSTING</h3>
-          </Button>
-        </div>
-        <div className="bg-primary-color rounded-lg  flex mt-5 justify-center items-center  mr-5 text-center w-60 h-60">
-          <Button  className="bg-secondary hover:border-2 hover:bg-background rounded-lg w-full h-full">
-            <Database className="mx-auto" size={50} />
-            <h3 className="mt-2">DATABASE</h3>
-          </Button>
-        </div>
+      <div className="flex justify-center items-center flex-wrap	 mx-auto w-full md:w-3/4">
+        <Link href={"/tools"}>
+          <div className="bg-primary-color rounded-lg  mt-5 flex  mr-5 w-60 h-60">
+            <Button className="bg-secondary hover:border-2  hover:bg-background rounded-lg w-full h-full">
+              <PanelsTopLeft size={50} className="mx-auto" />
+              WEBSITES
+            </Button>
+          </div>
+        </Link>
+        <Link href={"/tools"}>
+          <div className="bg-primary-color rounded-lg  mt-5  mr-5 w-60 h-60">
+            <Button className="bg-secondary hover:border-2 hover:bg-background rounded-lg w-full h-full">
+              <AppWindow size={50} className="mx-auto" />
+              <h3 className="mt-2">WEB APPS</h3>
+            </Button>
+          </div>
+        </Link>
+        <Link href={"/tools"}>
+          <div className="bg-primary-color rounded-lg   mt-5    mr-5 w-60 h-60">
+            <Button className="bg-secondary hover:border-2 hover:bg-background rounded-lg w-full h-full">
+              <Smartphone size={50} className="mx-auto" />
+              <h3 className="mt-2">MOBILE APPS</h3>
+            </Button>
+          </div>
+        </Link>
+        <Link href={"/tools"}>
+          <div className="bg-primary-color rounded-lg   mt-5  mr-5 w-60 h-60">
+            <Button className="bg-secondary hover:border-2 hover:bg-background rounded-lg w-full h-full">
+              <Mail size={50} className="mx-auto" />
+              <h3 className="mt-2">EMAIL</h3>
+            </Button>
+          </div>
+        </Link>
+        <Link href={"/tools"}>
+          <div className="bg-primary-color rounded-lg   mt-5   mr-5 w-60 h-60">
+            <Button className="bg-secondary hover:border-2 hover:bg-background rounded-lg w-full h-full">
+              <ShoppingBag size={50} className="mx-auto" />
+              <h3 className="mt-2">ECOMMERCE</h3>
+            </Button>
+          </div>
+        </Link>
+        <Link href={"/tools"}>
+          <div className="bg-primary-color rounded-lg   mt-5   mr-5 w-60 h-60">
+            <Button className="bg-secondary hover:border-2 hover:bg-background rounded-lg w-full h-full">
+              <Bot size={50} className="mx-auto" />
+              <h3 className="mt-2">
+                ARTIFICIAL
+                <br /> INTELLIGENCE
+              </h3>
+            </Button>
+          </div>
+        </Link>
+        <Link href={"/tools"}>
+          <div className="bg-primary-color rounded-lg   mt-5   mr-5 w-60 h-60">
+            <Button className="bg-secondary hover:border-2 hover:bg-background rounded-lg w-full h-full">
+              <Server size={50} className="mx-auto" />
+              <h3 className="mt-2">
+                WEBSITE <br />
+                HOSTING
+              </h3>
+            </Button>
+          </div>
+        </Link>
+        <Link href={"/tools"}>
+          <div className="bg-primary-color rounded-lg   mt-5   mr-5 w-60 h-60">
+            <Button className="bg-secondary hover:border-2 hover:bg-background rounded-lg w-full h-full">
+              <Database size={50} className="mx-auto" />
+              <h3 className="mt-2">DATABASE</h3>
+            </Button>
+          </div>
+        </Link>
       </div>
     </div>
   );
