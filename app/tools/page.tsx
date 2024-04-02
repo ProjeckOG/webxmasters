e03d51b42b025c5009a2f545972d6a6dd45be2e7
Davon Burks <burksdavon29@yahoo.com>
2024-04-01 23:34:55 -0400

    Fixinf buils

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -1,11 +1,10 @@
-import Dropdown from "./components/category-dropdown";
-import Filters from "./components/filters";
-
 export default function Tools() {
   return (
     <main className="">
-      <h1 className="text-center text-4xl font-bold my-10"> WEBMASTERS TOOLS LIST</h1>
-     
+      <h1 className="text-center text-4xl font-bold my-10">
+        {" "}
+        WEBMASTERS TOOLS LIST
+      </h1>
     </main>
-  )
+  );
 }
