7c8b35158c5d906947bd1c89d95c4aed6dd77128
Davon Burks <burksdavon29@yahoo.com>
2024-04-08 22:54:45 -0400

    Working on oauth Welcome Page

--- app/(Auth)/signup/components/googleAuth.tsx
+++ app/(Auth)/signup/components/googleAuth.tsx
@@ -14,7 +14,7 @@ export default function GoogleAuth() {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${origin}/auth/callback`,
+        redirectTo: `${origin}/auth/callback?next=/welcome`,
       },
     });
     if (error) {
