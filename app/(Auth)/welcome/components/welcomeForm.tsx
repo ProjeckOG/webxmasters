c0dca957a3d104e0be22d1bc696853fd6855fc9c
Davon Burks <burksdavon29@yahoo.com>
2024-04-09 22:35:17 -0400

    Still working on Welcome Page

--- app/(Auth)/welcome/components/welcomeForm.tsx
+++ app/(Auth)/welcome/components/welcomeForm.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client'
 import { Button } from "@/lib/@/components/ui/button";
 import {
   FormField,
@@ -13,125 +13,138 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
-import { redirect, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import supabase from "@/lib/utils/supabase/client";
+import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar";
 
-export default function WelcomeForm() {
-   const getUser = async () => {
-
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-  }
-  getUser()
-
+export default function WelcomeForm({ user }) {
+ 
   const MAX_UPLOAD_SIZE = 1024 * 1024 * 3; // 3MB
-  const ACCEPTED_FILE_TYPES = ['image/jpeg','image/jpg','image/png','image/webp'];
-  const router = useRouter()
+  const ACCEPTED_FILE_TYPES = [
+    "image/jpeg",
+    "image/jpg",
+    "image/png",
+    "image/webp",
+  ];
+  const router = useRouter();
   const formSchema = z.object({
-    avatar: z
-    .instanceof(File)
-    .optional()
-    .refine((file) => {
-      return !file || file.size <= MAX_UPLOAD_SIZE;
-    }, 'File size must be less than 3MB')
-    .refine((file) => {
-      return ACCEPTED_FILE_TYPES.includes(file.type);
-    }, 'File must be a jpg, png, jpeg, or webp'),
-    username: z
-      .string()
-      .min(3, "Your username must be 3 characters long")
-      .max(50),
-    birthdate: z
-    .date()
+    avatar_url: z.union([z.instanceof(File), z.null()]).optional().refine((file) => {
+      return !file || (file.size <= MAX_UPLOAD_SIZE && ACCEPTED_FILE_TYPES.includes(file.type));
+    }, {
+      message: "File must be less than 3MB and of type JPG, PNG, JPEG, or WEBP"
+    }),
+    username: z.string().min(3).max(50),
+    birthdate: z.string(),
   });
+  
 
-  const form = useForm<z.infer<typeof formSchema>>({
+  const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      avatar: user?.avatar_url || 'public/default-photo.png',
-      username: "",
-      birthdate: "",
+      username: user?.username || "",
+      birthdate: user?.birthdate || "",
     },
   });
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
+     
+      // Check if the user object is available and has an id
+      if (!user || !user.id) {
+        toast.error("User is not authenticated or user data is not available.");
+        return;  // Stop execution if there is no user data
+      }
       
       const { error } = await supabase
         .from("users")
-        .update({ username: values.username, birthdate: values.birthdate })
-        .eq("id", user.id);
-        router.push("/user")
+        .update({ avatar_url: values.avatar_url, username: values.username, birthdate: values.birthdate })
+        .eq("id", user.id);  // Now we are sure user and user.id exist
+  
+      if (error) throw error;
+      router.push("/user");
     } catch (error) {
-      // Handle errors (e.g., showing a message to the user)
       console.error(error);
-      // Optionally, update state to show an error message on the UI
+      toast.error(`Failed to update profile: ${error.message}`);
     }
   }
-  return (
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-          <FormField
-              control={form.control}
-              name="avatar"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Profile Picture</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="file"
-                      className="w-1/4 p-2  bg-primary-foreground hover:bg-secondary rounded-full"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Choose your Username!</FormLabel>
-                  <FormControl>
-                    <Input
-                    type="text"
-                      className="w-full p-2  bg-primary-foreground hover:bg-secondary rounded"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="birthdate"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Birthdate</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="date"
-                      className="w-full p-2  bg-primary-foreground hover:bg-secondary rounded"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+  
 
-            <Button
-              type="submit"
-              variant="outline"
-              className="flex  bg-secondary-color items-center p-8 w-full  rounded hover:bg-accent-color"
-            >
-              Finish Registration
-            </Button>
-          </form>
-        </Form>
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <Avatar>
+          <AvatarImage
+            src={user?.avatar_url || "/default-photo.png"}
+            alt="Profile Picture"
+            className="h-20 w-20 mx-auto mb-5 md:mb-0 rounded-full"
+          />
+          <AvatarFallback>Profile Picture</AvatarFallback>
+        </Avatar>
+        <FormField
+          control={form.control}
+          name="avatar_url"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Profile Picture</FormLabel>
+              <FormControl>
+                <Input
+                  type="file"
+                  accept={ACCEPTED_FILE_TYPES.join(',')}
+                  onChange={(e) => form.setValue('avatar_url', e.target.files[0] || null)}  // Manually handle file input
+                  className="w-full mx-auto p-2 bg-primary-foreground hover:bg-secondary rounded"
+              
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Choose your Username!</FormLabel>
+              <FormControl>
+                <Input
+                  type="text"
+                  className="w-full p-2 bg-primary-foreground hover:bg-secondary rounded"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="birthdate"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Birthdate</FormLabel>
+              <FormControl>
+                <Input
+                  type="date"
+                  className="w-full p-2 bg-primary-foreground hover:bg-secondary rounded"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          variant="outline"
+          className="flex items-center p-8 w-full rounded hover:bg-accent-color bg-secondary-color"
+        >
+          Finish Registration
+        </Button>
+      </form>
+    </Form>
   );
 }
+function uploadAvatar(arg0: any) {
+  throw new Error("Function not implemented.");
+}
+
--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,11 +1,34 @@
+'use client'
 import UserNav from "./components/user-nav";
 import TileNav from "./components/tile-nav";
 import supabase from "@/lib/utils/supabase/client";
+import { useState, useEffect } from "react";
 
 
-export default async function User() {
+export default  function User() {
   
-  const { data: { user } } = await supabase.auth.getUser()
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchUser() {
+      try {
+        const { data, error } = await supabase.auth.getUser();
+        if (error) throw error;
+        setUser(data.user);
+      } catch (error) {
+        console.error('Error fetching user:', error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchUser();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>; // Or any other loading state indicator
+  }
   return (
     <div className="mx-auto items-center mt-10 flex flex-col">
       <UserNav user={user} />
